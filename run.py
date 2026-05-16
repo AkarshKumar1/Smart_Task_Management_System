e2d70bc8a44ebc0729a274c2049feb4a4b216783
Akarsh Kumar <akarsh.kumar2.cs28@iilm.edu>
2026-05-17 04:05:17 +0530

    Update run.py

--- run.py
+++ run.py
@@ -24,7 +24,9 @@ if __name__ == "__main__":
     )
 
     socketio.run(
-        app,
-        host="0.0.0.0",
-        port=port
+      app,
+      host="0.0.0.0",
+      port=port,
+      debug=False,
+      allow_unsafe_werkzeug=True
     )
