1e4557b698ae00ad4de985720df51baaf7ff6c6e
Akarsh Kumar <akarsh.kumar2.cs28@iilm.edu>
2026-05-17 03:34:28 +0530

    Enable automatic database table creation
    Added automatic database table creation on app startup.

--- run.py
+++ run.py
@@ -1,13 +1,30 @@
+import os
+
 import pymysql
 
 pymysql.install_as_MySQLdb()
 
 from app import create_app, socketio
 
+from app.extensions import db
 
 app = create_app()
 
 
+# Create database tables automatically
+with app.app_context():
+
+    db.create_all()
+
+
 if __name__ == "__main__":
 
-    socketio.run(app, debug=True)
+    port = int(
+        os.environ.get("PORT", 5000)
+    )
+
+    socketio.run(
+        app,
+        host="0.0.0.0",
+        port=port
+    )
